903383e7f5c6d71f127742384160317fe1d5227b
Evgeniy Vlasov <79114158852@mail.ru>
2024-06-11 12:48:53 +0300

    Добавлен middleware проверки города

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::group(
+    [
+        'prefix' => '/{city?}',
+        'middleware' => \App\Http\Middleware\SetCity::class
+    ], 
+    function(){
+        Route::get('/', function () {
+            print_r(request()->all());
+        });
+    }
+);
+
+
